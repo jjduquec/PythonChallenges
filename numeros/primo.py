9cc5990377316b1a20828d2e1b6a6be3dfad0945
Juan Jose Duque <juanjo.dc@outlook.com>
2022-12-20 14:30:30 -0500

    factorial y primo resueltos

---
+++ numeros/primo.py
@@ -0,0 +1,10 @@
+def primo(numero):
+    
+    if numero <= 1 :  
+        return False 
+    
+    for i in range(2,numero):  
+        if numero % i ==0:
+            return False 
+    
+    return True
